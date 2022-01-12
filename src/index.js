4bdd6f3df4ae8d3e834b5c42d86fcc4a399704e9
andrewNEW42 <sanjino56@gmail.com>
2022-01-12 10:00:50 +0200

    fixed the import route for admin-user-info style file

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import "./styles/css/navigation-menu.css"
 import "./styles/css/pages/login-register-page.css"
 import "./styles/css/pages/user-page.css"
 import "./styles/css/pages/admin-page.css"
-import "./styles/css/pages/admin-console-page.css"
+import "./styles/css/pages/admin-user-info.css"
 import "./styles/css/pages/edit-task-page.css"
 import "./styles/css/pages/edit-user-profile.css";
 
